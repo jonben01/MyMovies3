0b116e6063e9e6f6d7ec6f1f7854d5c20a8d003c
Peteris Meiris <petmei01@easv365.dk>
2025-01-08 12:19:38 +0100

    fxml

--- src/main/java/dk/easv/mymovies3/GUI/Controllers/NewCategoryController.java
+++ src/main/java/dk/easv/mymovies3/GUI/Controllers/NewCategoryController.java
@@ -1,4 +1,14 @@
 package dk.easv.mymovies3.GUI.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class NewCategoryController {
+    @FXML
+    private void handleAddCategory(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void handleDeleteCategory(ActionEvent actionEvent) {
+    }
 }
